4afb8152c7b7d82f366e56bd52d58c1a86d6d158
Rafael Acosta <theGitNoob@gmail.com>
2024-05-31 16:39:20 -0400

    fix: fix error on user create data input

--- app/routers/users.py
+++ app/routers/users.py
@@ -12,7 +12,7 @@ from app.utils.auth import (
     get_current_active_user,
     create_access_token,
 )
-from app.utils.user_utils import create_user
+from app.utils.user_utils import create_user, get_user
 
 router = APIRouter(tags=["Auth"])
 
@@ -36,21 +36,18 @@ async def login(
 
 @router.post("/signup")
 async def signup(
-    form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
+    user_in: UserCreate,
     db: Session = Depends(get_db),
-) -> Token:
-    user = authenticate_user(form_data.username, form_data.password, db)
+) -> User:
+    user = get_user(user_in.username, db)
     if user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User already exists",
             headers={"WWW-Authenticate": "Bearer"},
         )
-    user_data = UserCreate(username=form_data.username, password=form_data.password)
-    _ = create_user(db, user_data)
-
-    access_token = create_access_token(data={"sub": form_data.username})
-    return Token(access_token=access_token, token_type="bearer")
+    new_user = create_user(db, UserCreate(**user_in.dict()))
+    return new_user
 
 
 @router.get("/users/me", response_model=User)
