ec783ca1f8a6a9c85cf8245142d391e9787c9043
Rafael Acosta <theGitNoob@gmail.com>
2024-05-31 21:18:41 -0400

    refactor: delete test_main

---
+++ app/tests/conf.py
@@ -0,0 +1,57 @@
+import pytest
+# from fastapi.testclient import TestClient
+from sqlalchemy import create_engine, StaticPool
+from sqlalchemy.orm import sessionmaker
+from starlette.testclient import TestClient
+
+from app.database import Base, get_db
+from app.main import app
+
+DATABASE_TEST_URL = "sqlite://"
+
+engine = create_engine(
+    DATABASE_TEST_URL,
+    connect_args={"check_same_thread": False},
+    poolclass=StaticPool,
+)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base.metadata.create_all(bind=engine)
+
+
+@pytest.fixture(scope="function")
+def db_session():
+    connection = engine.connect()
+    transaction = connection.begin()
+    session = TestingSessionLocal(bind=connection)
+    yield session
+    session.close()
+    transaction.rollback()
+    connection.close()
+
+
+@pytest.fixture(scope="function")
+def test_client(db_session):
+    def override_get_db():
+        try:
+            yield db_session
+        finally:
+            db_session.close()
+
+    app.dependency_overrides[get_db] = override_get_db
+    with TestClient(app) as test_client:
+        yield test_client
+
+
+#
+# def override_get_db():
+#     try:
+#         db = TestingSessionLocal()
+#         yield db
+#     finally:
+#         db.close()
+
+
+# app.dependency_overrides[get_db] = override_get_db
+
+# client = TestClient(app)
