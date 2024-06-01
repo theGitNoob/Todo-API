30e76e0731ad44103ee580dd967c771d99284510
Rafael Acosta <theGitNoob@gmail.com>
2024-05-31 21:32:41 -0400

    fix: change deprecated import

--- app/database.py
+++ app/database.py
@@ -2,8 +2,7 @@ import os
 
 from dotenv import load_dotenv
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
 
 load_dotenv()
 
